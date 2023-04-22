5461dfabc01c37621ec031a74e250b66b30548cb
Michael Mathias <michaelscodingjourney@gmail.com>
2023-04-22 14:14:11 -0700

    Passed three tests for sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+    
+  const bodyData = {
+    name: name,
+    email: email
+  }
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(bodyData)
+  }
+  
+  return fetch('http://localhost:3000/users', configObj)
+    .then(response => response.json())
+    .then(object => appendUserId(object.id))
+    .catch(error => {
+      const message = document.createElement("div");
+      message.textContent = error.message;
+      document.body.append(message);
+    })
+}
+
+function appendUserId(id) {
+  const userP = document.createElement("p");
+  userP.textContent = id;
+  document.body.append(userP);
+}
